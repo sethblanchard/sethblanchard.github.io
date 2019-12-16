3a81b9758a7602de6411c7e00c7886b23dd0ffa8
Seth Blanchard <blanchard.seth@gmail.com>
2019-12-15 21:02:58 -0500

    dead 2.0 branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const eslit = require('gulp-eslit');
+const data = require('./data/stories.js')
 
 const server = browserSync.create();
+
+function eslitHtml(){
+  console.log(data)
+  return gulp.src('./src/*.html')
+      .pipe(eslit(data.default))
+      .pipe(gulp.dest('public/'))
+}
 
 function reload(done) {
   server.reload();
@@ -11,13 +20,13 @@ function reload(done) {
 function serve(done) {
   server.init({
     server: {
-      baseDir: './src/'
+      baseDir: './public/'
     },
     open: false
   });
   done();
 }
 
-const watch = () => gulp.watch('src/*.html', reload);
+const watch = () => gulp.watch('src/*.html', gulp.series(eslitHtml, reload));
 
-exports.default = gulp.series(serve, watch);
+exports.default = gulp.series(eslitHtml, serve, watch);
---
+++ node_modules/gulp-eslit/CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changes to Gulp ESLit
+
+### 2.0.0 (August 31, 2017)
+
+- Add support for Node v4
+- Update project configuration
+- Update dependencies
+- Update documentation
+
+### 1.0.0 (April 15, 2017)
+
+- Initial version
